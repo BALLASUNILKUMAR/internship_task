96fd77377ac754a5f6ac8383f0e6e608c8c28694
Balla Sunil Kumar <87064284+BALLASUNILKUMAR@users.noreply.github.com>
2023-10-05 14:50:34 +0530

    Update registered.sql
    yes

--- registered.sql
+++ registered.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 26, 2022 at 03:25 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
